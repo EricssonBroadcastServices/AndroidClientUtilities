a4e1690338db68a13e138b8082e5aa52096e0cd5
João Coelho <admin.henyah@gmail.com>
2017-09-15 15:30:17 +0200

    Adding utilities.

---
+++ src/main/java/net/ericsson/emovs/utilities/DeviceInfo.java
@@ -0,0 +1,69 @@
+package net.ericsson.emovs.utilities;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+import java.io.Serializable;
+
+import static android.provider.Settings.Secure.ANDROID_ID;
+import static android.provider.Settings.Secure.getString;
+
+public class DeviceInfo implements Serializable {
+    private static final double TABLET_SIZE_TRESHOLD = 7;
+    private static final String FALLBACK_ID = "AndroidId";
+    
+    public String deviceId;
+    public int height;
+    public int width;
+    public String model;
+    public String name;
+    public String os;
+    public String osVersion;
+    public String manufacturer;
+    public String type;
+    
+    private static String getDeviceId(ContentResolver resolver) {
+        try {
+            String android_id = getString(resolver, ANDROID_ID);
+            if (android_id == null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+                    android_id = Build.getSerial();
+                }
+            }
+            if (android_id == null) {
+                android_id = FALLBACK_ID;
+            }
+            return android_id;
+        }
+        catch (Exception e) {
+           Logging.e("Error getting device id: " + e.toString());
+            return FALLBACK_ID;
+        }
+    }
+
+    private static boolean diagonalLargerThanSize(double width, double height, double diagonalTreshold) {
+        Logging.v(String.format("Width %f\" Height %f\" Diagonal Treshold %f\"",width, height, diagonalTreshold));
+        return width*width + height*height > diagonalTreshold*diagonalTreshold;
+    }
+
+    public static DeviceInfo collect(Context context) {
+        DeviceInfo info = new DeviceInfo();
+        info.model = Build.MODEL;
+        info.manufacturer = Build.MANUFACTURER;
+        info.deviceId = getDeviceId(context.getContentResolver());
+        info.os = "Android";
+        info.osVersion = Build.VERSION.RELEASE;
+        
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        info.width = metrics.widthPixels;
+        info.height = metrics.heightPixels;
+        
+        boolean isTablet = diagonalLargerThanSize(info.width/metrics.xdpi, info.height/metrics.ydpi, TABLET_SIZE_TRESHOLD);
+        info.type = isTablet ? "TABLET" : "MOBILE";
+        
+        return info;
+    }
+
+}
---
+++ src/main/java/net/ericsson/emovs/utilities/Logging.java
@@ -0,0 +1,49 @@
+package net.ericsson.emovs.utilities;
+
+import android.util.Log;
+
+public class Logging {
+   private static final String TAG = "Logging Lib: ";
+   private static final boolean LOG_ENABLED = true; //BuildConfig.DEBUG;
+
+   public static void d(String msg) {
+      if (LOG_ENABLED) {
+         Log.d(TAG, TAG + msg);
+      }
+   }
+
+   // Report to server
+//   public static void e(String title, Exception ex, boolean isReportToServer) {
+//      if (BuildConfig.DEBUG) {
+//         Log.e(TAG, TAG + title + ex.toString());
+//      }
+//
+//      if (isReportToServer) {
+//         Crashlytics.getInstance().core.logException(ex);
+//      }
+//   }
+
+   public static void e(String title, Exception ex) {
+      if (LOG_ENABLED) {
+         Log.e(TAG, TAG + title + ex.toString());
+      }
+   }
+
+   public static void e(String text) {
+      if (LOG_ENABLED) {
+         Log.e(TAG, TAG + text);
+      }
+   }
+
+   public static void i(String msg) {
+      if (LOG_ENABLED) {
+         Log.i(TAG, TAG + msg);
+      }
+   }
+
+   public static void v(String msg) {
+      if (LOG_ENABLED) {
+         Log.v(TAG, TAG + msg);
+      }
+   }
+}
