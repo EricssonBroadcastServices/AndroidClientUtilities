b1026fb7af300b3549a8f36abac118364c178fd2
João Coelho <admin.henyah@gmail.com>
2017-09-28 16:24:42 +0200

    Adding event hooks and working on event listener notification.

---
+++ src/main/java/com/ebs/android/utilities/ErrorCodes.java
@@ -0,0 +1,10 @@
+package com.ebs.android.utilities;
+
+/**
+ * Created by Joao Coelho on 2017-09-28.
+ */
+
+public class ErrorCodes {
+    public static final int EXO_PLAYER_INTERNAL_ERROR = 1;
+    public static final int EXPOSURE_ENTITLEMENT_ERROR = 2;
+}
---
+++ src/main/java/com/ebs/android/utilities/ErrorRunnable.java
@@ -0,0 +1,9 @@
+package com.ebs.android.utilities;
+
+/**
+ * Created by Joao Coelho on 2017-09-28.
+ */
+
+public interface ErrorRunnable {
+    void run(int errorCode, String errorMessage);
+}
