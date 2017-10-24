2f8e6057135826f44da19a926320b45805aa5362
João Coelho <admin.henyah@gmail.com>
2017-10-24 10:55:49 +0200

    Adding ContextRegistry

---
+++ src/main/java/net/ericsson/emovs/utilities/ContextRegistry.java
@@ -0,0 +1,19 @@
+package net.ericsson.emovs.utilities;
+
+import android.content.Context;
+
+/**
+ * Created by Joao Coelho on 2017-10-24.
+ */
+
+public class ContextRegistry {
+    private static Context context;
+
+    public static void bind(Context newContext) {
+        context = newContext;
+    }
+
+    public Context get() {
+        return context;
+    }
+}
