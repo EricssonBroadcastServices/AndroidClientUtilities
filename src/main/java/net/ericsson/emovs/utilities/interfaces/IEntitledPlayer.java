63b231d282970ba6e2e2c29580103c081ce5524c
João Coelho <admin.henyah@gmail.com>
2018-01-30 15:16:42 +0100

    Adding documentation for IEntitledPlayer.

--- src/main/java/net/ericsson/emovs/utilities/interfaces/IEntitledPlayer.java
+++ src/main/java/net/ericsson/emovs/utilities/interfaces/IEntitledPlayer.java
@@ -10,8 +10,26 @@ import net.ericsson.emovs.utilities.models.EmpProgram;
  */
 
 public interface IEntitledPlayer extends IPlayer {
+    /**
+     * Returns Entitlement for current playback.
+     * This object has important playback-related information, for example: manifest locator, contract restrictions, bookmark values, etc..
+     */
     Entitlement getEntitlement();
+
+    /**
+     * Returns current playable
+     */
     IPlayable getPlayable();
+
+    /**
+     * Returns current playback session ID
+     */
     String getSessionId();
+
+    /**
+     * Returns current program being played:
+     * - Differs from getPlayable because getPlayable returns the playable loaded by user
+     * - For instance when a program boundary is crossed, then a new program is issued
+     */
     EmpProgram getCurrentProgram();
 }
