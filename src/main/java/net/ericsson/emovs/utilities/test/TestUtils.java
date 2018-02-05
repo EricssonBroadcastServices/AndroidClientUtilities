7b35e87fd7d5a09b03ca12b20866f10ad9bf9bc7
João Coelho <admin.henyah@gmail.com>
2018-02-05 16:54:09 +0100

    TestUtils helper class.

---
+++ src/main/java/net/ericsson/emovs/utilities/test/TestUtils.java
@@ -0,0 +1,11 @@
+package net.ericsson.emovs.utilities.test;
+
+import net.ericsson.emovs.utilities.entitlements.IEntitlementCallback;
+
+/**
+ * Created by Joao Coelho on 2018-02-05.
+ */
+
+public class TestUtils {
+
+}
