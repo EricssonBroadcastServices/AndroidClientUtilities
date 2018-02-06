5f83924a8315a0f8dfdc92e994697a70bf26d2ab
João Coelho <admin.henyah@gmail.com>
2018-02-06 15:05:56 +0100

    Test utilities that help to mock the singleton providers.

--- src/main/java/net/ericsson/emovs/utilities/test/TestUtils.java
+++ src/main/java/net/ericsson/emovs/utilities/test/TestUtils.java
@@ -2,10 +2,32 @@ package net.ericsson.emovs.utilities.test;
 
 import net.ericsson.emovs.utilities.entitlements.IEntitlementCallback;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 /**
  * Created by Joao Coelho on 2018-02-05.
  */
 
 public class TestUtils {
 
+    public static void mockProvider(Class<?> myProviderClass, Object providerToMock) {
+        Class<?>[] innerClss = myProviderClass.getDeclaredClasses();
+        for (Class<?> innerCls : innerClss) {
+            try {
+                Field staticInstance = innerCls.getDeclaredField("sInstance");
+                staticInstance.setAccessible(true);
+
+                Field modifiersField = Field.class.getDeclaredField("modifiers");
+                modifiersField.setAccessible(true);
+                modifiersField.setInt(staticInstance, staticInstance.getModifiers() & ~Modifier.FINAL);
+
+                staticInstance.set(null, myProviderClass.cast(providerToMock));
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
