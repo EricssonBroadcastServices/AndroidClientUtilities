8effa79b1fb7d8bd2c956ce33ec10a8876167da6
João Coelho <admin.henyah@gmail.com>
2018-04-16 11:53:31 +0200

    Getting first image and title possible by default if no image is available for the defined locale.

--- src/main/java/net/ericsson/emovs/utilities/models/LocalizedMetadata.java
+++ src/main/java/net/ericsson/emovs/utilities/models/LocalizedMetadata.java
@@ -2,6 +2,7 @@ package net.ericsson.emovs.utilities.models;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
@@ -26,46 +27,95 @@ public class LocalizedMetadata implements Serializable {
         return this.descriptions.get(locale);
     }
 
+    public String getTitleQuick() {
+        if (this.titles.size() == 0 || !this.titles.values().iterator().hasNext()) {
+            return null;
+        }
+        return this.titles.values().iterator().next();
+    }
+
+    public String[] getTitleLocales() {
+        if (this.titles.size() == 0 || !this.titles.values().iterator().hasNext()) {
+            return null;
+        }
+        return this.titles.keySet().toArray(new String[this.titles.size()]);
+    }
+
     public String getTitle(String locale) {
         if (this.titles.containsKey(locale) == false) {
-            return null;
+            return getTitleQuick();
         }
         return this.titles.get(locale);
     }
 
-    public EmpImage getImage(String locale, String filterType) {
-        if (this.images.containsKey(locale) == false) {
+    public String[] getImageLocales() {
+        if (this.images.size() == 0 || !this.images.values().iterator().hasNext()) {
             return null;
+        }
+        return this.images.keySet().toArray(new String[this.images.size()]);
+    }
+
+    public EmpImage getImageQuick() {
+        if (this.images.size() == 0) {
+            return null;
+        }
+        for (ArrayList<EmpImage> images : this.images.values()) {
+            if (images == null || images.size() == 0) {
+                continue;
+            }
+            return images.get(0);
+        }
+        return null;
+    }
+
+    public EmpImage getImage(String _locale, String filterType) {
+        String locale = _locale;
+        if (this.images.containsKey(locale) == false) {
+            String[] locales = getImageLocales();
+            if (locales == null) {
+                return null;
+            }
+            locale = locales[0];
         }
         for (EmpImage image : this.images.get(locale)) {
             if (image.type.equals(filterType)) {
                 return image;
             }
         }
-        return null;
+        return getImageQuick();
     }
 
-    public EmpImage getImage(String locale, EmpImage.Orientation filterOrientation) {
+    public EmpImage getImage(String _locale, EmpImage.Orientation filterOrientation) {
+        String locale = _locale;
         if (this.images.containsKey(locale) == false) {
-            return null;
+            String[] locales = getImageLocales();
+            if (locales == null) {
+                return null;
+            }
+            locale = locales[0];
         }
         for (EmpImage image : this.images.get(locale)) {
             if (image.orientation == filterOrientation) {
                 return image;
             }
         }
-        return null;
+        return getImageQuick();
     }
 
-    public EmpImage getImage(String locale, EmpImage.Orientation filterOrientation, String filterType) {
+    public EmpImage getImage(String _locale, EmpImage.Orientation filterOrientation, String filterType) {
+        String locale = _locale;
         if (this.images.containsKey(locale) == false) {
-            return null;
+            String[] locales = getImageLocales();
+            if (locales == null) {
+                return null;
+            }
+            locale = locales[0];
         }
         for (EmpImage image : this.images.get(locale)) {
             if (image.orientation == filterOrientation && filterType.equals(image.type)) {
                 return image;
             }
         }
-        return null;
+        return getImageQuick();
     }
 }
